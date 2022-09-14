59ad4425b1e0f9ee553c2fb928591335ae6cfbb5
Srinivas Chary <srinivasa@aarth.io>
2022-09-14 17:29:39 +0530

    get values where in multiple depth of object

---
+++ getDepthOfValuesInObject.js
@@ -0,0 +1,17 @@
+const depthObject = {
+  name: "chary",
+  child: { name: "darci", child: { name: "JS | BUG" }, class: 50 },
+  class: 10,
+};
+const getValues = (ob, tmp) => {
+  for (x in ob) {
+    if (typeof ob[x] === "object") {
+      //   console.log(x);
+      getValues(ob[x], tmp);
+    } else {
+      tmp.push(ob[x]);
+    }
+  }
+  console.log(tmp);
+};
+getValues(depthObject, []);
