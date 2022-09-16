6665c7c76b0170e4c12e91290c698c80eb0a5018
Srinivas Chary <srinivasa@aarth.io>
2022-09-16 15:14:12 +0530

    fix spread operator real usage code

--- passByRefAndValue.js
+++ passByRefAndValue.js
@@ -8,7 +8,7 @@ const passByref = () => {
   peopleArray[1].name = "Joe";
   console.log(peopleArray); // i will override the name through the all objects in array REASON : because all objects in an array are referening in one memory code
 };
-// passByref();
+passByref();
 
 const passByValue = () => {
   const person = {
@@ -18,6 +18,39 @@ const passByValue = () => {
 
   const peopleArray = [{ ...person }, { ...person }, { ...person }];
   peopleArray[1].name = "Joe";
-  console.log(peopleArray);
+  console.log(peopleArray); // now this time spread operator created a new object for each time spread so every object have own memory address
 };
 passByValue();
+
+const spreadOperatorOverride = () => {
+  const carOne = {
+    color: "blue",
+    status: {
+      running: true,
+      passengers: 4,
+      wiperFluid: "empty",
+    },
+    age: 5,
+    miles: 50000,
+    value: "8000",
+  };
+
+  const carTwo = {
+    color: "green",
+    status: {
+      running: "yes",
+      passengers: 2,
+      fuelTank: "empty",
+    },
+    value: 9000,
+  };
+
+  const combinedCar = {
+    ...carOne,
+    ...carTwo,
+  };
+
+  console.log(combinedCar); // spread operator only spreads the latest depth not more the one depth
+  //    NOTE : if two variables have the same name then it overrides older one with latest one
+};
+spreadOperatorOverride();
