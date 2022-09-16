324c1247c34d8ffb735f391a6a790bf45f897bb7
Srinivas Chary <srinivasa@aarth.io>
2022-09-16 15:04:52 +0530

    fix pass by value code

--- passByRefAndValue.js
+++ passByRefAndValue.js
@@ -8,4 +8,16 @@ const passByref = () => {
   peopleArray[1].name = "Joe";
   console.log(peopleArray); // i will override the name through the all objects in array REASON : because all objects in an array are referening in one memory code
 };
-passByref();
+// passByref();
+
+const passByValue = () => {
+  const person = {
+    age: 20,
+    name: "Ben",
+  };
+
+  const peopleArray = [{ ...person }, { ...person }, { ...person }];
+  peopleArray[1].name = "Joe";
+  console.log(peopleArray);
+};
+passByValue();
