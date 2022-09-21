9469006b1e38478110e1e494ec92f82db10d3c15
Srinivas Chary <srinivasa@aarth.io>
2022-09-21 16:31:05 +0530

    fix merge duplicates into array and reduce objects

--- mergeDuplicatedPropertiesIntoArray.js
+++ mergeDuplicatedPropertiesIntoArray.js
@@ -23,3 +23,41 @@ const fun = (ar) => {
 };
 
 console.log(fun(data));
+// =====================================================================
+const arr = [
+  { valid_days: ["0"], timeslots: [{ from_time: "09:00", to_time: "12:00" }] },
+  { valid_days: ["1"], timeslots: [{ from_time: "09:00", to_time: "12:00" }] },
+  {
+    valid_days: ["2"],
+    timeslots: [
+      { from_time: "09:00", to_time: "12:00" },
+      { from_time: "13:00", to_time: "15:00" },
+    ],
+  },
+  {
+    valid_days: ["3"],
+    timeslots: [
+      { from_time: "09:00", to_time: "12:00" },
+      { from_time: "13:00", to_time: "15:00" },
+    ],
+  },
+];
+
+const fun1 = (ar) => {
+  const output = ar.reduce((prev, curr) => {
+    const tmp = prev.find(
+      (e) => JSON.stringify(e.timeslots) == JSON.stringify(curr.timeslots)
+    );
+    if (tmp) {
+      tmp.valid_days.push(...curr.valid_days);
+    } else {
+      prev.push({
+        timeslots: curr.timeslots,
+        valid_days: [...curr.valid_days],
+      });
+    }
+    return prev;
+  }, []);
+  return output;
+};
+console.log(fun1(arr));
